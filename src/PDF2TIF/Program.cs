aed40c130f5f8a886ac346326d961904a1618b89
Stefan Pelc <s.pelc@tme.co.at>
2021-04-06 15:05:46 +0200

    fixed pdf2tif by changing winapp to consoleapp and hide window

--- src/PDF2TIF/Program.cs
+++ src/PDF2TIF/Program.cs
@@ -1,37 +1,72 @@
 ﻿using GhostScriptWrapper;
 using System;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace PDF2TIF
 {
 	public static class Converter
 	{
+		[DllImport("kernel32.dll")]
+		static extern IntPtr GetConsoleWindow();
+
+		[DllImport("user32.dll")]
+		static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+		const int SW_HIDE = 0;
+		const int SW_SHOW = 5;
+
 		public static void Main(string[] args)
 		{
-			var errorMsg = "Expected two arguments, inputPath and outputPath: -input '{PathToFile}' -output '{PathToFile}\\{FileName + %04d.tif}'";
-			if (args.Length != 4)
-				throw new Exception(errorMsg);
-
-			var pdfFilePath = string.Empty;
-			var tifFilePath = string.Empty;
-
-			for (int i = 0; i < args.Length; i++)
+			try
 			{
-				var arg = args[i];
-				if (i + 1 < args.Length)
+				var handle = GetConsoleWindow();
+				ShowWindow(handle, SW_HIDE);
+
+				var errorMsg = "Expected two arguments, inputPath and outputPath: -input '{PathToFile}' -output '{PathToFile}\\{FileName + %04d.tif}'";
+				if (args.Length != 4)
+					throw new Exception(errorMsg);
+
+				var pdfFilePath = string.Empty;
+				var tifFilePath = string.Empty;
+
+				for (int i = 0; i < args.Length; i++)
 				{
-					if (arg.ToLower().Contains("input"))
-						pdfFilePath = args[i + 1].Trim().Replace("'", "");
-					else if (arg.ToLower().Contains("output"))
-						tifFilePath = args[i + 1].Trim().Replace("'", "");
+					var arg = args[i];
+					if (i + 1 < args.Length)
+					{
+						if (arg.ToLower().Contains("input"))
+							pdfFilePath = args[i + 1].Trim().Replace("'", "");
+						else if (arg.ToLower().Contains("output"))
+							tifFilePath = args[i + 1].Trim().Replace("'", "");
+					}
 				}
+
+				if (string.IsNullOrEmpty(pdfFilePath) || string.IsNullOrEmpty(tifFilePath))
+					throw new Exception(errorMsg);
+
+				var arguments = $" -sDEVICE=tiff24nc -sCompression=lzw -r300x300 -dNOPAUSE -sOutputFile={tifFilePath} {pdfFilePath}";
+				var argArray = arguments.Split(' ');
+				Wrapper.CallAPI(argArray);
 			}
+			catch (Exception ex)
+			{
+				var msg = GetMessage(ex);
+				var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "PDF2TIFError.txt");
+				if (File.Exists(filePath))
+					File.Delete(filePath);
+				File.WriteAllLines(filePath, new[] { msg });
+			}
+		}
 
-			if (string.IsNullOrEmpty(pdfFilePath) || string.IsNullOrEmpty(tifFilePath))
-				throw new Exception(errorMsg);
-
-			var arguments = $" -sDEVICE=tiff24nc -sCompression=lzw -r300x300 -dNOPAUSE -sOutputFile={tifFilePath} {pdfFilePath}";
-			var argArray = arguments.Split(' ');
-			Wrapper.CallAPI(argArray);
+		private static string GetMessage(Exception ex)
+		{
+			var msg = ex.Message;
+			if (ex is ExternalException eex)
+				msg = eex.ErrorCode + ": " + msg;
+			if (ex.InnerException != null)
+				return msg + " - " + GetMessage(ex.InnerException);
+			return msg;
 		}
 	}
 }
